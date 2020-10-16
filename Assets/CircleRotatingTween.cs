234d7acfa48e45296d543f76771255d9c7ef5106
hynuzurah <hynuzurah@gmail.com>
2020-10-16 13:05:04 +0300

    Tasks 1-2 completed

---
+++ Assets/CircleRotatingTween.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class CircleRotatingTween : MonoBehaviour
+{
+    [SerializeField] private Transform firstPart;
+    [SerializeField] private Transform secondPart;
+
+    private void Start()
+    {
+        PlayAnimation();
+    }
+    
+    public void PlayAnimation()
+    {
+        var sequence = DOTween.Sequence();
+        sequence.Append(firstPart.transform.DOLocalRotate(new Vector3(0f, 360, 0f), 1f, RotateMode.LocalAxisAdd));
+        sequence.Join(secondPart.transform.DOLocalRotate(new Vector3(360f, 0, 0f), 1f, RotateMode.LocalAxisAdd));
+        sequence.SetLoops(-1);
+    }
+}
---
+++ Assets/SwordLoadingTween.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class SwordLoadingTween : MonoBehaviour
+{
+    private void Start()
+    {
+        PlayAnimation();
+    }
+
+    // 65 -120 -20 -45
+    public void PlayAnimation()
+    {
+        var sequence = DOTween.Sequence();
+        sequence.Append(transform.DOLocalRotate(new Vector3(0, 0, 65f), 0.3f));
+        sequence.Append(transform.DOLocalRotate(new Vector3(0, 0, -180f), 0.3f, RotateMode.LocalAxisAdd));
+        sequence.Append(transform.DOLocalRotate(new Vector3(0, 0, -20f), 0.3f));
+        sequence.Append(transform.DOLocalRotate(new Vector3(0, 0, -45f), 0.3f));
+        sequence.SetLoops(-1);
+    }
+}
